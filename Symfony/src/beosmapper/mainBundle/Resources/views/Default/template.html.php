3949581a69a2d5519672db0d5370028f68f84536
kamyar68 <kamyar.hasanzadeh@gmail.com>
2013-10-18 23:41:30 +0300

    Template added

---
+++ Symfony/src/beosmapper/mainBundle/Resources/views/Default/template.html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Beosmapper!</title>
+    </head>
+    <body>
+        <h1>{{ "Besomapper Home" }}	</h1>
+		<p>
+    {{ "Beosmapper is an extension to " }}
+	<a href="http://blindsquare.com/">BlindSquare</a>
+	{{ "that indicates the building entrances and traffic lights for blind pedestrians.
+	This is a project by **Group 2** for the course GIS Application Development - Aalto University" }}
+
+	<img src="http://www.cug.edu.gh/images/page%20construction.jpg" alt="Page under construction"> 
+	</p>
+	</body>
+</html>

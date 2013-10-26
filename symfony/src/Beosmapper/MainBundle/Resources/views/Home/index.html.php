bc10918d2cb989b1aa2d81a76955a5d961df4c2a
Marco Martins <marcomartins@Marcos-MacBook-Pro-3.local>
2013-10-26 21:16:10 +0300

    Setting up AngularJS with Symphony

--- symfony/src/Beosmapper/MainBundle/Resources/views/Home/index.html.php
+++ symfony/src/Beosmapper/MainBundle/Resources/views/Home/index.html.php
@@ -1,25 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" ng-app>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-		<title><?php $view['slots']->output('title', 'Welcome to Beosmapper') ?></title>
+		<title><?php $view['slots']->output('title', 'Beosmapper') ?></title>
 
-		<?php $view['slots']->output('stylesheets') ?>
+		<?php /*$view['slots']->output('stylesheets')*/ ?>
 
 		<link rel="shortcut icon" href="<?php echo $view['assets']->getUrl('favicon.ico') ?>" />
+
+
+		<?php // Include Javascript files 
+		$header_js_files = $view['assetic']->javascripts( array( '@BeosmapperMainBundle/Resources/public/js/*' ) ); ?>
+
+		<?php foreach ( $header_js_files as $url ) { ?>
+
+			<script type="text/javascript" src="<?php echo $view->escape( $url ); ?>"></script>
+
+		<?php } ?>
+
 	</head>
 
 <body>
-	<?php echo "Besomapper Home"; ?>
+	<h1><?php echo "Besomapper Home"; ?></h1>
 
 	<p>Beosmapper is an extension to <a href="http://blindsquare.com/">BlindSquare</a> that indicates the building entrances and traffic lights for blind pedestrians.</p>
 	
 	<p>This is a project by **Group 2** for the course GIS Application Development - Aalto University</p>
 
-	<img src="http://www.cug.edu.gh/images/page%20construction.jpg" alt="Page under construction"> 
+	<p>Nothing here {{'yet' + '!'}}</p>
 
 	<?php $view['slots']->output('_content') ?>
-	<?php $view['slots']->output('javascripts') ?>
+
+	<div id="map"></div>
+	
 </body>
 </html>

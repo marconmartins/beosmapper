33c401bc639998669cbedcc5e33cbeb684ac9b7e
Marco Martins <marcomartins@wireless-86-50-136-153.open.aalto.fi>
2013-11-03 16:34:43 +0200

    Add marker to map

--- symfony/web/js/d169577.js
+++ symfony/web/js/d169577.js
@@ -5,7 +5,8 @@ app.controller( "MainController", function( $scope, $http ) {
 
 	osm_feature = "tourism:hotels";
 
-	var map, layer;
+	var map, layer, markers;
+
 	var x2js = new X2JS();
 
 	/**
@@ -19,14 +20,30 @@ app.controller( "MainController", function( $scope, $http ) {
 
 		layer = new OpenLayers.Layer.OSM( "Simple OSM Map" );
 
-		map.addLayer(layer);
+		map.addLayer( layer );
+
+		markers = new OpenLayers.Layer.Markers( 'Building entrances' );
+
+		map.addLayer( markers );
 
 		map.setCenter(
 			new OpenLayers.LonLat( -71.147, 42.472 ).transform(
 			new OpenLayers.Projection( "EPSG:4326" ),
 			map.getProjectionObject()
-			), 12
+			), 3
 		);
+
+	};
+
+
+	//addMarker = function( lonlat ) {
+
+		//markers.addMarker( new OpenLayers.Marker( lonlat ) );
+
+	//};
+
+
+	clearMarkers = function() {
 
 	};
 
@@ -38,12 +55,12 @@ app.controller( "MainController", function( $scope, $http ) {
 	 */
 	getOSMFeatures = function() {
 
-		var osm_url = "http://www.overpass-api.de/api/xapi?*[tourism=hotel][bbox=24.64233,60.11141,25.06531,60.27796]";
+		var osm_url = "http://www.overpass-api.de/api/xapi?*[building=entrance][bbox=24.64233,60.11141,25.06531,60.27796]";
 
 		$http( { method: 'GET', url: osm_url } ).
 		success( function( data, status, headers, config ) {
 
-			console.log( data );
+			//console.log( data );
 
 			console.log( x2js.xml_str2json( data ) );
 
@@ -54,12 +71,19 @@ app.controller( "MainController", function( $scope, $http ) {
 
 		});
 
+		var coords = new OpenLayers.LonLat( "6220211", "170892" );
+
+		markers.addMarker( new OpenLayers.Marker( coords ) );
+
 	};
+
+
+	// Initialize Page
+	initMap();
 
 	// Get OSM Features
 	getOSMFeatures();
 
-	// Initialize Page
-	initMap();
+
 
 });

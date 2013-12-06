8e2c29e2feb66d8ba5cc567ae95f1f7156298ff0
Marco Martins <marcomartins@Marcos-MacBook-Pro-3.local>
2013-12-06 19:08:45 +0200

    Frontend tweaks

--- symfony/src/Beosmapper/MainBundle/Resources/views/Home/index.html.php
+++ symfony/src/Beosmapper/MainBundle/Resources/views/Home/index.html.php
@@ -35,95 +35,82 @@
 <body ng-cloak >
 
 	<div id="content" ng-controller="MainController">
+
 	<?php $view['slots']->output('_content') ?>
 
     <div class="container">
 		<div class="row">
 			<div class="col-lg-16">
-			<div class="well text-center">
-				<h1>Welcome to Beosmapper!</h1>
-				<h3>Beosmapper is an extension to <a href="http://blindsquare.com/">BlindSquare</a> that indicates the building entrances and traffic lights for blind pedestrians.</h3>   
+				<div class="well text-center">
+
+					<h1>Welcome to Beosmapper!</h1>
+
+					<h3>Beosmapper is an extension to <a href="http://blindsquare.com/">BlindSquare</a> that indicates the building entrances and traffic lights for blind pedestrians.</h3>   
+
+				</div>
 			</div>
-			</div>
-		</div>
-      <div class="row">
-        <div class="col-lg-8">
-			<p></p>
-			<div id="map" style="height: 300px; width: 90%; "></div>
-			<p></p>
-			<a class="btn btn-primary btn-lg" href="" ng-click="locate()">Show my current location</a>
 		</div>
 
-		<hr>
+      <div id="submission-area" class="row">
+
+        <div id="map-block" class="col-lg-8">
+
+			<div id="map"></div>
+
+			<a class="btn btn-primary btn-sm" href="" ng-click="locate()">Show my current location</a>
+		</div>
+
+		<div id="information-block" class="col-lg-4">
+
+			<h3>Feature Information</h3>
+
+			<div class="form-group">
 		
-		<div class="col-lg-4">
+				<select id="feature-type" class="form-control has-error">
 
-			<h3>Add new entry</h3>
+					<option value="">1. Choose the feature type</option>
 
-			<div class="btn-group">
+					<option value="entrance">Building Entrance</option>
 
-				<button type="button" class="btn btn-danger">1. Choose entry type</button>
+					<option value="tlight">Traffic light</option>
 
-				<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
+					<option value="Stairs">Stairs</option>
 
-					<span class="caret"></span>
-
-					<span class="sr-only">Toggle Dropdown</span>
-
-				</button>
-
-				<ul class="dropdown-menu" role="menu">
-
-					<li><a href="#">Entrance</a></li>
-
-					<li><a href="#">Traffic light</a></li>
-
-					<li><a href="#">Stairs</a></li>
-
-				</ul>
+				</select>
 
 			</div>
 			
-			<div class="dropdown">
-				<button class="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
-				Dropdown
-					<span class="caret"></span>
-				</button>
-				<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-					<li role="presentation" class="divider"></li>
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-				</ul>
-			</div>
-
-			<hr>
 
 			<div class="input-group">
 
 				<span class="input-group-addon">2. Describe entry</span>
 
-				<input type="text" class="form-control" placeholder="Your description" ng-model='entry.description' >
+				<input type="text" class="form-control has-error" placeholder="Your description" ng-model='entry.description' >
 
 			</div>
 
-			<hr>
 
-			<div class="input-group">
-				<button type="submit" class="btn btn-primary" ng-click='submitEntry()'>Submit</button>
-			</div>
-			<h3>Registration</h3>
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Username" ng-model='entry.login.username'><p></p>
-				<input type="text" class="form-control" placeholder="Password" ng-model='entry.login.password'>
-			</div>
-			
-			<hr>
+			<div class="form-group">
 
+				<h5>3. Add your OpenStreetMap Login</h5>
+				<p class="text-muted"><small><small>Don't have a OpenStreetMap account? Sign up <a href="https://www.openstreetmap.org/user/new" target="_blank">here</a>.</small></small></p>
+
+				<label class="sr-only" for="osm_password">OpenStreetMap Username</label>
+				<input type="text" id="username" class="form-control" placeholder="Username" ng-model='entry.login.username'><p></p>
+
+				<label class="sr-only" for="osm_password">OpenStreetMap Password</label>
+				<input type="text" id="osm_password" class="form-control has-error" placeholder="Password" ng-model='entry.login.password'>
+
+				<p class="text-info"><small><small>Note: Your OpenStreetMap login information will not be stored, it will be used only to add the data.</small></small></p>
+
+			</div>
 			
-			
-			
+			<div class="input-group pull-right">
+
+				<button type="submit" class="btn btn-default" ng-click='submitEntry()'>Submit</button>
+
+			</div>
+
 		</div>
 	</div>
 
